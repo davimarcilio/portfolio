97d2abcac91e899c755ca2ffe6472300ff635853
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-30 16:54:17 -0300

    meta og, vlw maykão

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <title>Davi Marcilio</title>
+        <meta
+          property="og:title"
+          content="Davi Marcilio - Full-Stack Developer"
+          key="title"
+        />
+        <meta
+          property="og:image"
+          content="https://uploaddeimagens.com.br/imagens/rVD38UY"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:image:alt"
+          content="Davi Marcilio - Full Stack Developer"
+        />
+        <meta
+          property="og:description"
+          content="JavaScript, TypeScript Full Stack Developer"
+        />
+      </Head>
       <body className="bg-stone-900">
         <Main />
         <NextScript />
